047fe8c03975c3dc0bf7f5ba65e84f7ba60a86ba
Morgan Donze <donzemorgan@gmail.com>
2020-10-30 12:15:19 -0400

    Make card editable

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
 import { Card } from "../../App";
 
@@ -8,16 +8,23 @@ export default function CardComponent(props: {
   onPressEdit(id: number | null): any;
 }) {
   const { card, editing, onPressEdit } = props;
+  const [content, setContent] = useState(card.content);
 
   if (editing) {
     return (
       <View
         style={{ padding: 20, backgroundColor: "#f4efef", marginBottom: 20 }}
       >
-        <TextInput style={{ borderWidth: 1, borderColor: "#333" }} />
+        <TextInput
+          style={{ borderWidth: 1, borderColor: "#333" }}
+          onChangeText={(text) => {
+            setContent(text);
+          }}
+        />
         <Button
           title="Done"
           onPress={() => {
+            card.content = content;
             onPressEdit(null);
           }}
         />
