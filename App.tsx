bc96588d2c3c3e42f9ed6325b2c2c4a0536217a4
Morgan Donze <donzemorgan@gmail.com>
2020-10-30 11:40:00 -0400

    Add CardStore

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { action, makeObservable, observable } from "mobx";
+
+interface Card {
+  id: number;
+  content: string;
+}
+
+class CardStore {
+  cards: Card[] = [];
+  constructor() {
+    makeObservable(this, {
+      cards: observable,
+      getCards: action,
+    });
+  }
+
+  getCards() {
+    return this.cards;
+  }
+
+  addCard(content: string) {
+    this.cards.push({ content, id: this.cards.slice().length });
+  }
+}
+
+const cardStore = new CardStore();
+cardStore.addCard("Test Card");
+cardStore.addCard("Test Card");
+
+function CardComponent(props: { card: Card }) {
+  const { card } = props;
+  return (
+    <View style={{padding: 20, backgroundColor: "#f4efef", marginBottom: 20}}>
+      <Text>{card.content}</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={cardStore.cards.slice()}
+        renderItem={({ item: card }) => <CardComponent card={card} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +54,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
